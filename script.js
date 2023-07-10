60ceab71b4aee79a20e03346919023dbd3a92a9e
ernest1298 <97247914+ernest1298@users.noreply.github.com>
2023-07-10 17:21:30 -0600

    ss

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById('resumeButton').addEventListener('click', function() {
+    document.getElementById('resumeContent').style.display = 'block';
+  });
+  
+  document.getElementById('closeButton').addEventListener('click', function() {
+    document.getElementById('resumeContent').style.display = 'none';
+  });
